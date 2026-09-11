0b7809e9c29a0912038c31f68a739be3424240d5
SOMilJAIn12 <sj.somiljain2006@gmail.com>
2026-09-11 17:07:06 +0530

    toolcalling added

---
+++ toolcalling.py
@@ -0,0 +1,7 @@
+from dotenv import load_dotenv
+load_dotenv()
+from langchain_groq import ChatGroq
+from langchain.tools import tool 
+from langchain_core.messages import HumanMessage
+from rich import print 
+

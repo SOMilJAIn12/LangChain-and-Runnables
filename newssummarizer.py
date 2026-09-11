674be7c7e179b8bd7ab32ed9fae3126a7f65f9b8
SOMilJAIn12 <sj.somiljain2006@gmail.com>
2026-09-11 14:19:28 +0530

    news summarizer edited

--- newssummarizer.py
+++ newssummarizer.py
@@ -7,13 +7,17 @@ from langchain_core.output_parsers import StrOutputParser
 search_tool=TavilySearchResults(max_result=5)
 model = ChatGroq(model="openai/gpt-oss-20b")
 prompt = ChatPromptTemplate.from_template(
-    """
+    '''
 Yor are a helpful ai assistent
 summarize the following news into clear bullet points
 {news}
-"""
+'''
 )
 chain= prompt | model |StrOutputParser()
 news_result= search_tool.run("latest news of 2026")
 news=chain.invoke({"news": news_result})
 print(news)
+
+print(search_tool.name)
+print(search_tool.description)
+print(search_tool.args)

453c70b7e0e3a0a18cae2f1e80a82d27967549ff
SOMilJAIn12 <sj.somiljain2006@gmail.com>
2026-09-12 13:45:49 +0530

    toolcalling under progress

--- toolcalling.py
+++ toolcalling.py
@@ -2,6 +2,16 @@ from dotenv import load_dotenv
 load_dotenv()
 from langchain_groq import ChatGroq
 from langchain.tools import tool 
-from langchain_core.messages import HumanMessage
 from rich import print 
-
+model = ChatGroq(model="openai/gpt-oss-20b")
+def get_text_length(text:str)->int:
+    """Returns number of character in given text"""
+    return len(text)
+#tool binding
+llm_with_tool=model.bind_tools([get_text_length])
+result=model.invoke("hello")
+result2=llm_with_tool.invoke("hello")
+print(result)
+print()
+print()
+print(result2)

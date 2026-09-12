4a6f7c485f2edcce08c1b63741b5e33788bfeb7b
SOMilJAIn12 <sj.somiljain2006@gmail.com>
2026-09-12 15:01:00 +0530

    tool file changed

--- toolcalling.py
+++ toolcalling.py
@@ -1,20 +1,36 @@
 from dotenv import load_dotenv
 load_dotenv()
-from langchain_groq import ChatGroq
-from langchain.tools import tool 
-from rich import print 
-model = ChatGroq(model="openai/gpt-oss-20b")
-def get_text_length(text:str)->int:
-    """Returns number of character in given text"""
-    return len(text)
-#tool binding
-llm_with_tool=model.bind_tools([get_text_length])
 
-result=llm_with_tool.invoke("Returns number of character in given text : 'how are you'")
+from langchain_groq import ChatGroq
+from langchain.tools import tool
+from rich import print
+
+model = ChatGroq(model="openai/gpt-oss-20b")
+
+
+@tool
+def get_text_length(text: str) -> int:
+    """Returns number of characters in given text."""
+    return len(text)
+
+
+# Tool binding
+llm_with_tool = model.bind_tools([get_text_length])
+
+result = llm_with_tool.invoke(
+    "Returns number of characters in given text: 'how are you'"
+)
 
 if result.tool_calls:
-    tool_call=result.tool_calls[0]
-tool_name=tool_call["name"]
-tool_args=tool_call["args"]
-tool_result=get_text_length.invoke(tool_args)
-response=llm_with_tool.invoke(f"the length of text is{tool_result}")
+    tool_call = result.tool_calls[0]
+
+    tool_name = tool_call["name"]
+    tool_args = tool_call["args"]
+
+    tool_result = get_text_length.invoke(tool_args)
+
+    final_response = llm_with_tool.invoke(
+        f"The length of the text is {tool_result}"
+    )
+
+    print(final_response)

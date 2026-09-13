78e73d13dfe4da8e747b1d1e8736a34ff8f187f0
SOMilJAIn12 <sj.somiljain2006@gmail.com>
2026-09-13 15:45:26 +0530

    Weather agent cpmleted

--- Agents.py
+++ Agents.py
@@ -7,10 +7,21 @@ from langchain.tools import tool
 from langchain_core.messages import HumanMessage,ToolMessage
 from tavily import TavilyClient
 ## lets create tool
-
-def get_weather(city:str)->str:
-    """Get current weather of the city"""
-    API_KEY = os.getenv("OPENWEATHER_API_KEY")
+@tool
+def get_weather(city: str) -> str:
+    """Get current weather of a city"""
+    
+    api_key = os.getenv("OPENWEATHER_API_KEY")
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city},IN&appid={api_key}&units=metric"
+    
     response = requests.get(url)
     data = response.json()
+    
+    if str(data.get("cod")) != "200":
+        return f"Error: {data.get('message', 'Could not fetch weather')}"
+    
+    temp = data["main"]["temp"]
+    desc = data["weather"][0]["description"]
+    
+    return f"Weather in {city}: {desc}, {temp}°C"
+

9f8fb6f4303b41c3279043631dd17aa39ca45bec
SOMilJAIn12 <sj.somiljain2006@gmail.com>
2026-09-13 15:58:08 +0530

    news agent added

--- Agents.py
+++ Agents.py
@@ -25,3 +25,32 @@ def get_weather(city: str) -> str:
     
     return f"Weather in {city}: {desc}, {temp}°C"
 
+tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+@tool
+def get_news(city: str) -> str:
+    """Get latest news about a city"""
+    
+    response = tavily_client.search(
+        query=f"latest news in {city}",
+        search_depth="basic",
+        max_results=3
+    )
+    
+    results = response.get("results", [])
+    
+    if not results:
+        return f"No news found for {city}"
+    
+    news_list = []
+    
+    for r in results:
+        title = r.get("title", "No title")
+        url = r.get("url", "")
+        snippet = r.get("content", "")
+        
+        news_list.append(
+            f"- {title}\n   {url}\n   {snippet[:100]}..."
+        )
+    
+    return f"Latest news in {city}:\n\n" + "\n\n".join(news_list)
